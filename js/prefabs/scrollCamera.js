6962b98d187441cbd31a94c3af91ed71ad3efe1c
jjcapellan <soycape@hotmail.com>
2019-04-14 17:10:56 +0200

    Added inertia to the scroll

--- js/prefabs/scrollCamera.js
+++ js/prefabs/scrollCamera.js
@@ -1,62 +1,100 @@
 class ScrollingCamera extends Phaser.Cameras.Scene2D.Camera {
-    constructor(scene, x, y, width, height, { top, bottom, pointerDrag, wheel, wheelDrag }) {
+    constructor(scene, x, y, width, height, { top, bottom, wheel, drag, minSpeed }) {
         super(x, y, width, height);
-        this.top = top;
-        this.wheel = wheel || false;
+        // Public members
         this.scene = scene;
-        this.bottom = bottom - this.height;
-        this.rectangle = new Phaser.Geom.Rectangle(x, y, width, height);
-        this.verticalSpeed = 0;
-        this.pointerDrag = pointerDrag || 1;
-        this.wheelDrag = wheelDrag || 1;
-        this.scrollY = this.top;
         this.x = x;
         this.y = y;
         this.width = width;
-        this.height = height;        
+        this.height = height;
+        this.top = top;
+        this.bottom = (bottom) ? (bottom - this.height) : null;
+        this.wheel = wheel || false;
+        this.drag = drag || 0.95;
+        this.minSpeed = minSpeed || 5;
         this.init();
     }
 
     init() {
+        //// Private members               
+        this.scrollY = this.top || this.y;
+        this._rectangle = new Phaser.Geom.Rectangle(this.x, this.y, this.width, this.height);
+        // Vertical speed in pixels per second
+        this._speed = 0;
+        // scrollY value when drag action begins
+        this._startY = this.scrollY;
+        // scrollY value when drag action ends
+        this._endY = this.scrollY;
+        // timeStamp when drag action begins
+        this._startTime = 0;
+        // timeStamp when drag action ends
+        this._endTime = 0;
+
+        //// Sets events
         this.setDragEvent();
         if (this.wheel) {
             this.setWheelEvent();
         }
     }
 
-    setDrag(value) {
-        this.pointerDrag = value;
-        Phaser.Math.Clamp(this.pointerDrag, 0.05, 1);
+    setSpeed() {
+        let t = this;
+        let distance = t._endY - t._startY; // pixels
+        let duration = (t._endTime - t._startTime) / 1000; //seconds
+        this._speed = distance / duration; // pixels/second
     }
 
     setDragEvent() {
         this.scene.input.on('pointermove', this.dragHandler, this);
+        this.scene.input.on('pointerup', this.upHandler, this);
+        this.scene.input.on('pointerdown', this.downHandler, this);
     }
 
     setWheelEvent() {
         window.addEventListener('wheel', this.wheelHandler.bind(this));
     }
 
+    downHandler() {
+        this._startY = this.scrollY;
+        this._startTime = performance.now();
+    }
+
     dragHandler(pointer) {
         if (pointer.isDown && this.isOver(pointer)) {
-            this.scrollY -= (pointer.position.y - pointer.prevPosition.y) * this.pointerDrag;
-            this.clampScroll();
+            this.startY = this.scrollY;
+            this.scrollY -= (pointer.position.y - pointer.prevPosition.y);
         }
+    }
+
+    upHandler() {
+        this._endY = this.scrollY;
+        this._endTime = performance.now();
+        this.setSpeed();
     }
 
     wheelHandler(event) {
         if (this.isOver(this.scene.input.activePointer)) {
-            this.scrollY += event.deltaY * this.wheelDrag;
-            this.clampScroll();
+            this.scrollY += event.deltaY;
         }
     }
 
     isOver(pointer) {
-        return this.rectangle.contains(pointer.x, pointer.y);
+        return this._rectangle.contains(pointer.x, pointer.y);
     }
 
     clampScroll() {
         this.scrollY = Phaser.Math.Clamp(this.scrollY, this.top, this.bottom);
+        this._endY = this.scrollY;
+    }
+
+    update(time, delta) {
+        this.scrollY += this._speed * (delta / 1000);
+        this._speed *= this.drag;
+        if (Math.abs(this._speed) < this.minSpeed) {
+            this._speed = 0;
+        }
+        this.clampScroll();
+
     }
 
     destroy() {
@@ -73,6 +111,6 @@ class ScrollingCamera extends Phaser.Cameras.Scene2D.Camera {
         this.scene = null;
         this.scaleManager = null;
         this.sceneManager = null;
-        
+
     }
 }
