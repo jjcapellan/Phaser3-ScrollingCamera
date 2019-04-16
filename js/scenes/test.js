4a2d9126afdb652020f24c3fc863339b0965aa87
jjcapellan <soycape@hotmail.com>
2019-04-16 22:20:03 +0200

    New layout

--- js/scenes/test.js
+++ js/scenes/test.js
@@ -4,41 +4,52 @@ class Test extends Phaser.Scene {
   }
 
   create() {
+    this.makeContent();
+    this.makeGraphics();
+    this.makeScrollCamera();
+  }
+
+  makeContent() {
     let top = this.game.config.height;
     let gameWidth = this.game.config.width;
     let centerX = gameWidth / 2;
-    let marginX = 50;
     let marginY = 50;
     let paddingY = 50;
+    let rowsQty = 50;
     // Rows
     let textStyle = { fontFamily: 'Arial', fontSize: 26, color: '#ffffff' };
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < rowsQty; i++) {
       let y = top + marginY + i * paddingY;
       this.add.text(centerX, y, `--- ${y} ---`, textStyle)
         .setOrigin(0.5);
     }
 
-    // Guide line
+  }
+
+  makeGraphics() {
     let g = this.add.graphics();
     g.lineStyle(2, 0xdd0000);
-    g.lineBetween(marginX, 300, centerX - 100, 300);
-    g.lineBetween(centerX + 100, 300, gameWidth - marginX, 300);
+    g.strokeRect(45, 45, 310, 510);
+    g.fillStyle(0xdd0000, 1);
+    g.fillTriangle(80, 290, 80, 310, 120, 300);
+    g.fillTriangle(320, 290, 320, 310, 280, 300);
+  }
 
+  makeScrollCamera() {
     let camera1 = new ScrollingCamera(
       this,
-      0,
-      0,
-      gameWidth,
-      600,
+      50,
+      50,
+      300,
+      500,
       {
         top: 600,
         bottom: 3025,
-        wheel: true,
-        snapGrid: {
-          topMargin: 50,
-          padding: 50
-        }
+        wheel: true
       });
     this.cameras.addExisting(camera1);
+    camera1.scrollX = camera1.getScroll(this.game.config.width/2, 0).x;
   }
+
+
 }
